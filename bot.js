d50852cc5c2474007016adf527ec6e8e18f9377c
iiMagdyPro <45244143+iiMagdyPro@users.noreply.github.com>
2019-07-06 17:57:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['593743439544320008'];
 const db = require('quick.db');
 const premium = ['470896018603376640']
 const client = new Discord.Client();   
@@ -404,7 +404,7 @@ client.on('message', message => {
                 timestamp: new Date(),
                 footer: {
                   icon_url: client.user.avatarURL,
-                  text: "3bker Community ©"
+                  text: "M7md Ux"
                 }
             }}
             )
